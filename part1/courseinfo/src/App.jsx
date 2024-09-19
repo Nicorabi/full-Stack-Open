a6e963843d86f3646e2753feb101c02316024f24
Nicorabi <121673697+Nicorabi@users.noreply.github.com>
2024-09-19 23:38:45 +1000

    Added unicafe and updated courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -44,11 +44,11 @@ const App = () => {
   }
 
   return (
-    <div>
+    <>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </div>
+    </>
   )
 }
 
